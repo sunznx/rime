3526d7106531100dcc83311a0eade7127f84803f
amzxyz <rcm@qq.com>
2025-12-24 19:09:39 +0800

    dict: 词库调整

--- .github/workflows/scripts/release-build.sh
+++ .github/workflows/scripts/release-build.sh
@@ -35,10 +35,13 @@ package_schema_base() {
   rsync -av --ignore-existing \
     --exclude='/.*' \
     --exclude='/dist/' \
+    --exclude='/backgrounds' \
+    --exclude='/fonts' \
     --exclude='/release-please-config.json' \
     --exclude='/pro-*-fuzhu-dicts' \
     --exclude='/chaifen' \
     --exclude='/CHANGELOG.md' \
+    --exclude='/简纯+.trime.yaml' \
     --exclude='/custom' \
     --exclude='/LICENSE' \
     --exclude="/$OUT_BASE" \
@@ -96,10 +99,13 @@ package_schema_pro() {
     --exclude='/.*' \
     --exclude='/dist/' \
     --exclude='/dicts' \
+    --exclude='/backgrounds' \
+    --exclude='/fonts' \
     --exclude='release-please-config.json' \
     --exclude='pro-*-fuzhu-dicts' \
     --exclude='wanxiang_t9.schema.yaml' \
     --exclude='CHANGELOG.md' \
+    --exclude='/简纯+.trime.yaml' \
     --exclude='wanxiang.dict.yaml' \
     --exclude='wanxiang.schema.yaml' \
     --exclude='custom' \
